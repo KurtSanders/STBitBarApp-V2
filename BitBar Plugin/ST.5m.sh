c7a89eca33954df25e33a7ea3d1611245bc34a56
Kurt Sanders <kurt@kurtsanders.com>
2017-11-01 17:34:27 -0400

    Version 2.4: Fixed web proxy that holds an outdated version of the file in its cache

--- BitBar Plugin/ST.5m.sh
+++ BitBar Plugin/ST.5m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 2.3
+# Version 2.4
 if [ $# -gt 0 ]
   then
   	if [ $1 = "request" ]
@@ -12,8 +12,11 @@ if [ $# -gt 0 ]
         exit
     elif [ $1 = "github_ST_Python_Logic" ]
     then
-        curl -s --output ~/Downloads/ST_Python_Logic.py --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST_Python_Logic.py
-osascript -e 'display notification "Move the ST_Python_Logic.py from the Downloads Directory to BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '        open ~/Downloads/
+        osascript -e 'display notification "Downloading the ST_Python_Logic.py from KurtSanders STBitBarApp GitHub Repo " with title "ST BitBar App Notice" sound name "Frog" '
+        curl -s -H 'Cache-Control: no-cache' --output ~/Downloads/ST_Python_Logic.py --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST_Python_Logic.py
+        open ~/Downloads/
+        sleep 5
+        osascript -e 'display notification "Move the ST_Python_Logic.py from the Downloads Directory to BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '
         exit
 	fi
 fi
