401809d393a90e358634b61043f94f92777e00ae
Kurt Sanders <kurt@kurtsanders.com>
2018-01-21 20:12:27 -0500

    Version 2.32 - Fixed GitHub Old File Download Bug
    Changed bash file ‘Download Logic’ to insure the latest version of
    files are downloaded from GitHub.

--- BitBar Plugin/ST.5m.sh
+++ BitBar Plugin/ST.5m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 2.31
+# Version 2.32
 if [ $# -gt 0 ]
   then
   	if [ $1 = "request" ]
@@ -13,22 +13,32 @@ if [ $# -gt 0 ]
     elif [ $1 = "github_ST_Python_Logic" ]
     then
         osascript -e 'display notification "Downloading the ST_Python_Logic.py from KurtSanders STBitBarApp GitHub Repo " with title "ST BitBar App Notice" sound name "Frog" '
-        curl -s -H 'Cache-Control: no-cache' --output ~/Downloads/ST_Python_Logic.py --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST_Python_Logic.py
+        temp_file=$(mktemp)
+        curl -LkSs https://github.com/KurtSanders/STBitBarApp-V2/archive/master.zip -o "$temp_file"
+        unzip -qq -j -o -d ~/Downloads/ "$temp_file" 'STBitBarApp-V2-master/BitBar Plugin/ST/ST_Python_Logic.py'
+        rm "$temp_file"
+        chmod +x ST_Python_Logic.py
+        #        curl -s -H 'Cache-Control: no-cache' --output ~/Downloads/ST_Python_Logic.py --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST_Python_Logic.py
         open ~/Downloads/
         sleep 5
-        osascript -e 'display notification "Using a Text Editor, Copy & Paste the contents of the ST_Python_Logic.py in your Downloads Directory." with title "ST BitBar App Notice" sound name "Frog" '
+        osascript -e 'display notification "Move the new ST_Python_Logic.py located in your Downloads Directory." with title "ST BitBar App Notice" sound name "Frog" '
         sleep 2
-        osascript -e 'display notification "into the same file in your BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '
+        osascript -e 'display notification "into the same folder in your BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '
         exit
     elif [ $1 = "github_ST5MSH" ]
     then
         osascript -e 'display notification "Downloading the ST.5m.sh from KurtSanders STBitBarApp GitHub Repo " with title "ST BitBar App Notice" sound name "Frog" '
-        curl -s -H 'Cache-Control: no-cache' --output ~/Downloads/ST.5m.sh --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST.5m.sh
+#        curl -s -H 'Cache-Control: no-cache' --output ~/Downloads/ST.5m.sh --URL https://raw.githubusercontent.com/kurtsanders/STBitBarApp/master/BitBar%20Plugin/ST/ST.5m.sh
+        temp_file=$(mktemp)
+        curl -LkSs https://github.com/KurtSanders/STBitBarApp-V2/archive/master.zip -o "$temp_file"
+        unzip -qq -j -o -d ~/Downloads/ "$temp_file" 'STBitBarApp-V2-master/BitBar Plugin/ST.5m.sh'
+        rm "$temp_file"
+        chmod +x ST.5m.sh
         open ~/Downloads/
         sleep 5
-        osascript -e 'display notification "Using a Text Editor, Copy & Paste the contents of the ST.5m.sh in your Downloads Directory" with title "ST BitBar App Notice" sound name "Frog" '
+        osascript -e 'display notification "Move the new ST.5m.sh located in your Downloads Directory" with title "ST BitBar App Notice" sound name "Frog" '
         sleep 2
-        osascript -e 'display notification "into the same file in your BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '
+        osascript -e 'display notification "into the same folder in your BitBar Plugins Subdirectory" with title "ST BitBar App Notice" sound name "Frog" '
         exit
 	fi
 fi
